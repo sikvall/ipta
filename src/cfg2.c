9a90cebc10f9b8b0932618c71cf160f587a520c9
Anders Sikvall <anders@sikvall.se>
2015-05-17 19:17:06 +0200

    Just some finishing touches to make it look nice.

--- src/cfg2.c
+++ src/cfg2.c
@@ -15,70 +15,74 @@
 #include <ctype.h>
 #include "cfg2.h"
 
-/* This helper function added by Anders "Ichimusai" Sikvall to deal with
-   trimming of white space at the beginning and end of strings to be able to
-   cope with configuration files and make the following two lines to behave
-   as equivalent:
+/* This helper function added by Anders "Ichimusai" Sikvall although I
+   do not claim authorship to this code as it is likely to be very
+   similar to other such code snippets since donkey years ago...
+
+   I wanted to deal with trimming of white space at the beginning and
+   end of strings to be able to cope with configuration files and make
+   the following two lines to behave as equivalent:
 
    key=value
    key = value
 
-   Just as well as other varieties, such as indentation and other things in
-   the key/value pair that I think is sanity to get rid of.
+   Just as well as other varieties, such as indentation and other
+   things in the key/value pair that I think is sanity to get rid of.
 
    PARAMS
 
-     char *str
-         String to trim whitespace at beginning and end from.
+         char *str 
+                 String to trim whitespace at beginning and end from.
 
    RETURNS
 
-     The start of the string which should always be the same address as the
-     string inserted to trim. This is done in this way because I don't want
-     to screw up any malloc()/free() pairs for the strings.
+         The start of the string which should always be the same
+         address as the string inserted to trim. This is done in this
+         way because I don't want to screw up any malloc()/free()
+         pairs for the strings.
 */
 
 char *trimwhitespace(char *str)
 {
-    size_t len = 0;
-    char *frontp = str;
-    char *endp = NULL;
+	size_t len = 0;
+	char *frontp = str;
+	char *endp = NULL;
+	
+	if(str == NULL) 
+		return NULL;
+	
+	if(str[0] == '\0') 
+		return str;
+	
+	len = strlen(str);
+	endp = str + len;
+	
+	/* Move the front and back pointers to address the first
+	   non-whitespace characters from each end. */
+	while( isspace(*frontp) ) { ++frontp; }
+	if(endp != frontp) {
+		while( isspace(*(--endp)) && endp != frontp ) {}
+	}
+	
+	if( str + len - 1 != endp )
+		*(endp + 1) = '\0';
+	else if( frontp != str &&  endp == frontp )
+		*str = '\0';
+	
+	/* Shift the string so that it starts at str so that if it's
+	   dynamically allocated, we can still free it on the returned
+	   pointer. Note the reuse of endp to mean the front of the
+	   string buffer now. */
+	endp = str;
+	if(frontp != str) {
+		while( *frontp ) { *endp++ = *frontp++; }
+		*endp = '\0';
+	}
 
-    if( str == NULL ) { return NULL; }
-    if( str[0] == '\0' ) { return str; }
-
-    len = strlen(str);
-    endp = str + len;
-
-    /* Move the front and back pointers to address the first non-whitespace
-     * characters from each end.
-     */
-    while( isspace(*frontp) ) { ++frontp; }
-    if( endp != frontp )
-    {
-        while( isspace(*(--endp)) && endp != frontp ) {}
-    }
-
-    if( str + len - 1 != endp )
-            *(endp + 1) = '\0';
-    else if( frontp != str &&  endp == frontp )
-            *str = '\0';
-
-    /* Shift the string so that it starts at str so that if it's dynamically
-     * allocated, we can still free it on the returned pointer.  Note the reuse
-     * of endp to mean the front of the string buffer now.
-     */
-    endp = str;
-    if( frontp != str )
-    {
-            while( *frontp ) { *endp++ = *frontp++; }
-            *endp = '\0';
-    }
-
-
-    return str;
+	/* All done, return the pointer */
+	return str;
 }
-  
+
 
 void cfg_cache_clear(cfg_t *st)
 {
@@ -144,7 +148,8 @@ static cfg_uint32 cfg_hash_get(cfg_char *str)
 	if (str)
 		while(*str) {
 			/* or multiple with addition and bit shifting:
-			 * hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+			 * hash += (hash << 1) + (hash << 4) + (hash
+			 * << 7) + (hash << 8) + (hash << 24);
 			*/
 			hash *= hash;
 			hash ^= *str++;
@@ -441,10 +446,11 @@ cfg_error_t cfg_parse_buffer(cfg_t *st, cfg_char *buf, cfg_int sz)
 	if (ret > 0)
 		return ret;
 
-	/* find the number of keys by going trough the entire buffer initially.
-	 * we use this method instead of a growing list, since realloc can end up
-	 * being slow. even if our estimate is off a little realloc will have to copy
-	 * all pointer values for the buffer to grow */
+	/* find the number of keys by going trough the entire buffer
+	 * initially. we use this method instead of a growing list,
+	 * since realloc can end up being slow. even if our estimate
+	 * is off a little realloc will have to copy all pointer
+	 * values for the buffer to grow */
 	ls = bp = st->buf;
 	bend = bp + sz;
 	n = nkeys = 0;
