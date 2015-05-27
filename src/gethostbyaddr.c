ce3e37f09a3cca9e5ebde191b1877c120430ea8b
Anders Sikvall <anders@sikvall.se>
2015-05-27 20:23:32 +0200

    Fixed formatting issues.

--- src/gethostbyaddr.c
+++ src/gethostbyaddr.c
@@ -46,53 +46,54 @@
  ***********************************************************************/
 
 int get_host_by_addr(char *ip_address, char *hostname, int maxlen) {
-  struct sockaddr_in ip4addr;
-  int len = 0;
-  char host[NI_MAXHOST];        // Holding hostname
-  char service[NI_MAXSERV];     // Holding service
-  int retval = RETVAL_OK;
-  int dns_reply = 0;
+	struct sockaddr_in ip4addr;
+	int len = 0;
+	char host[NI_MAXHOST];        // Holding hostname
+	char service[NI_MAXSERV];     // Holding service
+	int retval = RETVAL_OK;
+	int dns_reply = 0;
 
-  /* Check if the answer is in the cache */
+	/* Check if the answer is in the cache */
 
-  /* TODO */
+	/* TODO */
 
-  /* Clear all stuff */
-  memset(&ip4addr, 0, sizeof(struct sockaddr_in));
+	/* Clear all stuff */
+	memset(&ip4addr, 0, sizeof(struct sockaddr_in));
 
-  /* Set up family and port */
-  ip4addr.sin_family = AF_INET;
-  ip4addr.sin_port = htons(0);
+	/* Set up family and port */
+	ip4addr.sin_family = AF_INET;
+	ip4addr.sin_port = htons(0);
 
-  /* Convert the address from string to AF_INET proper address and
-     populate structure with it. */
-  inet_pton(AF_INET, ip_address, &ip4addr.sin_addr);
+	/* Convert the address from string to AF_INET proper address and
+	   populate structure with it. */
+	inet_pton(AF_INET, ip_address, &ip4addr.sin_addr);
 
-  /* Call the DNS subsystem */
-  dns_reply = getnameinfo((struct sockaddr *) &ip4addr, sizeof(struct sockaddr_in), 
-			  host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
+	/* Call the DNS subsystem */
+	dns_reply = getnameinfo((struct sockaddr *) &ip4addr, sizeof(struct sockaddr_in), 
+				host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
 
-  /* If we got a name then we copy the name to maxlen characters into
-     the hostname pointer provided by the call. */
-  if (dns_reply == 0) {
-    len = strlen(host);
-    if(len > maxlen) {
-      strcpy(host, host+(len-maxlen));
-      host[0] = '*'; /* If the name is too long for the display field,
-			insert * to indicate that */
-    }
-    sprintf(hostname, "%s", host);
-    return retval;
-  }  
-  else {
+	/* If we got a name then we copy the name to maxlen characters into
+	   the hostname pointer provided by the call. */
+	if (dns_reply == 0) {
+		len = strlen(host);
+		if(len > maxlen) {
+			strcpy(host, host+(len-maxlen));
+			host[0] = '*'; /* If the name is too long for the
+					  display field, insert * to indicate
+					  that */
+		}
+		sprintf(hostname, "%s", host);
+		return retval;
+	}  
+	else {
       
-    /* If not, then we return the actual IP address. We also signal
-       that we could not look it up with a "1" as RETVAL Return the
-       address cut so that it fits the field length */
-    
-    strncpy(hostname, ip_address, maxlen);
-    
-    return RETVAL_NONAME; /* Nor regarded as a fatal error, just a signal in
-			     RETVAL that we did not look up a name */
-  }
+		/* If not, then we return the actual IP address. We also
+		   signal that we could not look it up with a "1" as RETVAL
+		   Return the address cut so that it fits the field length */
+		strncpy(hostname, ip_address, maxlen);
+
+		return RETVAL_NONAME; /* Nor regarded as a fatal error, just
+					 a signal in RETVAL that we did not
+					 look up a name */
+	}
 }
